c18de2668f4b2c7b4873f7fb27c7673039f62408
babylin <minlin@ucdavis.edu>
2018-10-04 01:22:14 -0700

    created service, second component problem detail in progress

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+
+import { routing } from  './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProblemListComponent
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ week1/oj-client/src/app/services/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Problem } from '../models/problem.model';
+import { PROBLEMS } from '../monk-problems';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+	problems: Problem[] = PROBLEMS;
+
+
+  constructor() { }
+  getProblems(): Problem[] {
+  	return this.problems;
+  }
+
+  getProblem(id: number): Problem {
+  	return this.problems.find( (problem) => problem.id === id );
+  }
+
+}
