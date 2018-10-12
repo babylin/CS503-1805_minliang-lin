fa3271e4a4de450538b50fe279a3cf08471a08b2
babylin <minlin@ucdavis.edu>
2018-10-11 23:16:28 -0700

    refatoring problem-list.component.ts by using subscripiton

--- week2/oj-client/src/app/components/problem-list/problem-list.component.ts
+++ week2/oj-client/src/app/components/problem-list/problem-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Problem } from '../../models/problem.model';
 import { DataService } from '../../services/data.service';
 
@@ -42,8 +43,9 @@ const PROBLEMS: Problem[] = [
   templateUrl: './problem-list.component.html',
   styleUrls: ['./problem-list.component.css']
 })
-export class ProblemListComponent implements OnInit {
+export class ProblemListComponent implements OnInit, OnDestroy {
 	problems: Problem[];
+	subscriptionProblem: Subscription;
 
   constructor(private dataService: DataService) { }
 
@@ -51,8 +53,14 @@ export class ProblemListComponent implements OnInit {
   	this.getProblems();
   }
 
+  ngOnDestroy() {
+  	this.subscriptionProblem.unsubscribe();
+  }
+
   getProblems() {
-  	this.problems = this.dataService.getProblems();
+  	// this.problems = this.dataService.getProblems();
+  	this.subscriptionProblem = this.dataService.getProblems()
+  		.subscribe(problems => this.problems = problems);
   }
 
 }
